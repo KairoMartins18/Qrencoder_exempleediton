935d4fc4f74d83ca64114b2fd173db0383ab9792
Kairo Martins <kairo.martins18@hotmail.com>
2021-05-11 18:12:51 -0400

    Adicionar arquivos de projeto.

---
+++ Form1.Designer.vb
@@ -0,0 +1,116 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.ColImg = New System.Windows.Forms.DataGridViewImageColumn()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.BackColor = System.Drawing.SystemColors.ButtonShadow
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.Location = New System.Drawing.Point(26, 29)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(209, 225)
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(534, 29)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(120, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "New qr"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.ColImg})
+        Me.DataGridView1.Location = New System.Drawing.Point(265, 29)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(240, 225)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(534, 186)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(120, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "print"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(534, 85)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(120, 23)
+        Me.Button3.TabIndex = 4
+        Me.Button3.Text = "add to datagrid"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'PrintDocument1
+        '
+        '
+        'ColImg
+        '
+        Me.ColImg.HeaderText = "Qr code"
+        Me.ColImg.ImageLayout = System.Windows.Forms.DataGridViewImageCellLayout.Zoom
+        Me.ColImg.Name = "ColImg"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(694, 296)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "Form1"
+        Me.Text = "Example"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents ColImg As DataGridViewImageColumn
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+End Class
---
+++ Form1.vb
@@ -0,0 +1,56 @@
+﻿Imports QRCoder
+Imports QRCoder.PayloadGenerator
+Public Class Form1
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles DataGridView1.CellContentClick
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Dim rowCount As Integer = 3
+        Dim colCount As Integer = 5
+        ' Add columns to DataGridView
+        For i As Integer = 0 To colCount - 1
+            Dim ColImage As New DataGridViewImageColumn
+            ColImage.Name = "ColImg" + i.ToString
+            ColImage.HeaderText = "Your Image" + i.ToString
+            DataGridView1.Columns.Add(ColImage)
+        Next
+        ' Add rows to DataGridView
+        For i As Integer = 0 To rowCount - 1
+            DataGridView1.Rows.Add()
+        Next
+        ' Assign values to DataGridView
+        For i As Integer = 0 To colCount - 1
+            For j As Integer = 0 To rowCount - 1
+                Dim Img As New DataGridViewImageCell
+                Img.Value = PictureBox1.BackgroundImage
+                DataGridView1(i, j) = Img
+            Next
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        Dim printDialog As PrintDialog = New PrintDialog()
+        printDialog.Document = PrintDocument1
+        If printDialog.ShowDialog() = DialogResult.OK Then
+            PrintDocument1.Print()
+        End If
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim qrGenerator As QRCodeGenerator = New QRCodeGenerator()
+        Dim qrCodeData As QRCodeData = qrGenerator.CreateQrCode("Hello world", QRCodeGenerator.ECCLevel.Q)
+        Dim qrCode As QRCode = New QRCode(qrCodeData)
+        Dim qrCodeImage As Bitmap = qrCode.GetGraphic(20)
+        PictureBox1.BackgroundImage = qrCodeImage
+    End Sub
+
+    Private Sub PrintDocument1_PrintPage(sender As Object, e As Printing.PrintPageEventArgs) Handles PrintDocument1.PrintPage
+
+        Dim rowIndex As Integer = DataGridView1.CurrentCell.RowIndex
+        Dim bm As Bitmap = CType(DataGridView1.Rows(rowIndex).Cells(0).Value, Bitmap)
+        e.Graphics.DrawImage(bm, 0, 0)
+    End Sub
+End Class
